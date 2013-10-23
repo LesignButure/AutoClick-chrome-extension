a8ef24eaecd5f333dbfe403b0b249f926249de21
LesignButure <lesignbuture@gmail.com>
2013-10-23 23:48:23 +0800

    first commit

---
+++ AutoClick/autoclick.js
@@ -0,0 +1,21 @@
+﻿$(function(){
+
+ 	var secondIframe = document.getElementsByTagName('iframe')[1];
+   	if(secondIframe != null){
+		var frameDoc = secondIframe.contentDocument || secondIframe.contentWindow.document;
+		var btn = frameDoc.getElementById("wscontinue");
+		var trigger = document.createEvent("Event");
+		trigger.initEvent("click", true, true);
+		btn.dispatchEvent(trigger);
+	}
+
+/*
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('test.js');
+(document.head||document.documentElement).appendChild(s);
+s.onload = function() {
+    s.parentNode.removeChild(s);
+};
+*/
+
+});
